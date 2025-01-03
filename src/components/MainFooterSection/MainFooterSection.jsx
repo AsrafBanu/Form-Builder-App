61c9e7e8f64b75813aee43718d2dbad2d01be691
AsrafBanu <asrafbanuashru@gmail.com>
2025-01-03 15:22:08 +0530

    Update

--- src/components/MainFooterSection/MainFooterSection.jsx
+++ src/components/MainFooterSection/MainFooterSection.jsx
@@ -21,26 +21,30 @@ const MainFooterSection = () => {
   return (
     <footer className="mainFooter">
       {/* Logo and Brand Section */}
-      <div className="mainFooter__header">
-        <img src={logo} alt="FormBot Logo" className="mainFooter__logo" />
-        <h4 className="mainFooter__brand">FormBot</h4>
-        <p className="mainFooter__credit">Made with ❤️ by Cuvette</p>
-      </div>
-
-      {/* Footer Columns Section */}
-      <div className="mainFooter__container">
-        {footerColumns.map((column, index) => (
-          <div key={index} className="mainFooter__column">
-            <h4 className="mainFooter__title">{column.title}</h4>
-            <ul className="mainFooter__list">
-              {column.links.map((link, linkIndex) => (
-                <li key={linkIndex} className="mainFooter__item">
-                  <a href="#" className="mainFooter__link">{link}</a>
-                </li>
-              ))}
-            </ul>
+      <div className="mainFooter__top">
+        <div className="mainFooter__brandSection">
+          <img src={logo} alt="FormBot Logo" className="mainFooter__logo" />
+          <div>
+            <h4 className="mainFooter__brand">FormBot</h4>
+            <p className="mainFooter__credit">Made with ❤️ by Cuvette</p>
           </div>
-        ))}
+        </div>
+
+        {/* Footer Columns Section */}
+        <div className="mainFooter__columns">
+          {footerColumns.map((column, index) => (
+            <div key={index} className="mainFooter__column">
+              <h4 className="mainFooter__title">{column.title}</h4>
+              <ul className="mainFooter__list">
+                {column.links.map((link, linkIndex) => (
+                  <li key={linkIndex} className="mainFooter__item">
+                    <a href="#" className="mainFooter__link">{link}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );
