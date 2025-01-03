80eb02990534c23e73ee0aae2a90129890e9af47
AsrafBanu <asrafbanuashru@gmail.com>
2025-01-03 14:49:32 +0530

    Update

--- src/components/MainFooterSection/MainFooterSection.jsx
+++ src/components/MainFooterSection/MainFooterSection.jsx
@@ -1,51 +1,68 @@
-import React from 'react';
-import './MainFooterSection.css';
-import logo from '../../assets/images/logo.svg';
-
-const MainFooterSection = () => {
-  const footerColumns = [
-    {
-      title: 'Product',
-      links: ['Status', 'Documentation', 'Roadmap', 'Pricing']
-    },
-    {
-      title: 'Community',
-      links: ['Discord', 'GitHub repository', 'Twitter', 'LinkedIn', 'OSS Friends']
-    },
-    {
-      title: 'Company',
-      links: ['About', 'Contact', 'Terms of Service', 'Privacy Policy']
-    }
-  ];
-
-  return (
-    <footer className="mainFooter">
-      {/* Logo and Title Section */}
-      <div className="mainFooter__header">
-        <img src={logo} alt="Triangle" className="mainFooter__logo" />
-        <h4 className="mainFooter__brand">FormBot</h4>
-        <p className="mainFooter__credit">Made with ❤️ by Cuvette</p>
+<footer class="mainFooter">
+  <div class="mainFooter__container">
+    <div class="mainFooter__column">
+      <div class="mainFooter__brand">
+        <img
+          src="formbot-logo.png"
+          alt="FormBot Logo"
+          class="mainFooter__brand-logo"
+        />
+        <p>Made with ❤️ by @cuvette</p>
       </div>
-
-      {/* Footer Columns */}
-      <div className="mainFooter__container">
-        {footerColumns.map((column, index) => (
-          <div key={index} className="mainFooter__column">
-            <h3 className="mainFooter__title">{column.title}</h3>
-            {column.links.length > 0 && (
-              <ul className="mainFooter__list">
-                {column.links.map((link, linkIndex) => (
-                  <li key={linkIndex} className="mainFooter__item">
-                    <a href="#" className="mainFooter__link">{link}</a>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        ))}
-      </div>
-    </footer>
-  );
-};
-
-export default MainFooterSection;
+    </div>
+    <div class="mainFooter__column">
+      <h4 class="mainFooter__title">Product</h4>
+      <ul class="mainFooter__list">
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Status</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Documentation</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Roadmap</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Pricing</a>
+        </li>
+      </ul>
+    </div>
+    <div class="mainFooter__column">
+      <h4 class="mainFooter__title">Community</h4>
+      <ul class="mainFooter__list">
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Discord</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">GitHub repository</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Twitter</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">LinkedIn</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">OSS Friends</a>
+        </li>
+      </ul>
+    </div>
+    <div class="mainFooter__column">
+      <h4 class="mainFooter__title">Company</h4>
+      <ul class="mainFooter__list">
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">About</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Contact</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Terms of Service</a>
+        </li>
+        <li class="mainFooter__item">
+          <a href="#" class="mainFooter__link">Privacy Policy</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</footer>
