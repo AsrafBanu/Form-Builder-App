f96a54f58b166755fafbbbeacf8e1a81f9936de2
AsrafBanu <asrafbanuashru@gmail.com>
2025-01-03 12:06:31 +0530

    Update

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -4,12 +4,7 @@ import Navbar from '../components/Navbar';
 import '../styles/Home.css';
 import MainNav from '../components/MainNav/MainNav';
 import DetailSection from '../components/DetailSection/DetailSection';
-import HomeFormSection from '../components/HomeFormSection/HomeFormSection';
-import MoreInfoSection from '../components/MoreInfoSection/MoreInfoSection';
-import OurPartnerSection from '../components/OurPartnerSection/OurPartnerSection';
-import FeatureSection from '../components/FeatureSection/FeatureSection';
-import LovedSection from '../components/LovedSection/LovedSection';
-import ContactUsSection from '../components/ContactUsSection/ContactUsSection';
+
 import MainFooterSection from '../components/MainFooterSection/MainFooterSection';
 
 export default function Home() {
@@ -18,12 +13,7 @@ export default function Home() {
       {/* <Navbar /> */}
       <MainNav />
       <DetailSection />
-      <HomeFormSection />
-      <MoreInfoSection />
-      <OurPartnerSection />
-      <FeatureSection />
-      <LovedSection />
-<ContactUsSection />
+
 <MainFooterSection/>      
         </div>
   );
