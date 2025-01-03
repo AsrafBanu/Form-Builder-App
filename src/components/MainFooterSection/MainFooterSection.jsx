4c2bf771d200cc212d905b25420c5db00c8668fd
AsrafBanu <asrafbanuashru@gmail.com>
2025-01-03 14:38:12 +0530

    Update

--- src/components/MainFooterSection/MainFooterSection.jsx
+++ src/components/MainFooterSection/MainFooterSection.jsx
@@ -2,17 +2,8 @@ import React from 'react';
 import './MainFooterSection.css';
 import logo from '../../assets/images/logo.svg';
 
-
-
 const MainFooterSection = () => {
   const footerColumns = [
-    
-    {
-
-      
-      title: 'Made with ❤️ by Cuvette',
-      links: []
-    },
     {
       title: 'Product',
       links: ['Status', 'Documentation', 'Roadmap', 'Pricing']
@@ -28,16 +19,18 @@ const MainFooterSection = () => {
   ];
 
   return (
-    
     <footer className="mainFooter">
+      {/* Logo and Title Section */}
+      <div className="mainFooter__header">
+        <img src={logo} alt="Triangle" className="mainFooter__logo" />
+        <h4 className="mainFooter__brand">FormBot</h4>
+        <p className="mainFooter__credit">Made with ❤️ by Cuvette</p>
+      </div>
 
-      
+      {/* Footer Columns */}
       <div className="mainFooter__container">
-      
         {footerColumns.map((column, index) => (
           <div key={index} className="mainFooter__column">
-            
-<div><img src={logo} alt="Triangle" /></div> <div><h4>FormBot</h4></div> 
             <h3 className="mainFooter__title">{column.title}</h3>
             {column.links.length > 0 && (
               <ul className="mainFooter__list">
